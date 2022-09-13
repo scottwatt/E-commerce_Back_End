84ab104cdabb001b0d1d9c0b063057f3a8decea3
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-09-13 09:10:44 -0700

    adds definitions to category.js and product.js

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true 
+    },
+    category_name: {
+      type: DataTypes.String,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      refrences: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
