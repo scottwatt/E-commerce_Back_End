caa2c3d9080a5788e61a5b38808cdf06665f472e
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-09-13 09:21:40 -0700

    finishes the models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: 'ProductTag'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  through: 'ProductTag'
+})
 module.exports = {
   Product,
   Category,
